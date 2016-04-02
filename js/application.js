f8c5e0bb82ef89096c394e1b7d37636b5982ac0b
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-04-02 11:54:58 +0900

    implement media player when tapped

--- js/application.js
+++ js/application.js
@@ -14,6 +14,7 @@ App.onLaunch = function(options) {
       resourceLoader.loadResource(`${options.BASEURL}templates/RWDevConTemplate.xml.js`,
         function(resource) {
         var doc = Presenter.makeDocument(resource);
+        doc.addEventListener("select", Presenter.load.bind(Presenter));
         Presenter.pushDocument(doc);
       });
     } else {
